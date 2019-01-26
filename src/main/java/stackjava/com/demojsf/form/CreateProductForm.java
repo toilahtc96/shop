38286d8014affc84cbf03e06cea26a6716a0536b
HaiHiep <37411724+HaiHiep@users.noreply.github.com>
2019-01-26 16:12:32 +0700

    jjjjj

---
+++ src/main/java/stackjava/com/demojsf/form/CreateProductForm.java
@@ -0,0 +1,10 @@
+package stackjava.com.demojsf.form;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class CreateProductForm {
+
+}
